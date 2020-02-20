5174ddd58100bfd35ca58f541966ab285f600b59
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-02-20 13:45:12 +0100

    Module login added

--- app/src/main/java/com/sdos/commerce/ui/MainActivity.kt
+++ app/src/main/java/com/sdos/commerce/ui/MainActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import com.sdos.commerce.R
+import com.sdos.commerce.ui.fragments.SplashFragmentListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SplashFragmentListener {
 
     private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSplahLoaded() {
+        navController.navigate(R.id.loginFragment)
     }
 }
--- app/src/main/java/com/sdos/commerce/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/sdos/commerce/ui/fragments/SplashFragment.kt
@@ -1,16 +1,29 @@
 package com.sdos.commerce.ui.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.sdos.commerce.R
+
+interface SplashFragmentListener {
+    fun onSplahLoaded()
+}
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
 
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        
+    }
+
+    companion object {
+        const val DELAY = 5
+    }
 }
