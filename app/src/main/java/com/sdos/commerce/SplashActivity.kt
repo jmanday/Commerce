45834406d57103fc91a1bf6b26c81bdeba88a1a3
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-02-20 13:09:45 +0100

    Log added

--- app/src/main/java/com/sdos/commerce/SplashActivity.kt
+++ app/src/main/java/com/sdos/commerce/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.sdos.commerce
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
