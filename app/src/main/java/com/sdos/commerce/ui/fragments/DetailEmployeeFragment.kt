0834e07552a8779a217e477394280d3e7e550fe1
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-02-25 17:43:05 +0100

    FragmentDetailEmployeeBinding added

--- app/src/main/java/com/sdos/commerce/ui/fragments/DetailEmployeeFragment.kt
+++ app/src/main/java/com/sdos/commerce/ui/fragments/DetailEmployeeFragment.kt
@@ -7,15 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import androidx.databinding.DataBindingUtil
 
 import com.sdos.commerce.R
+import com.sdos.commerce.databinding.FragmentDetailEmployeeBinding
 import com.sdos.commerce.ui.views.DateDialogView
 import com.sdos.commerce.ui.views.DateDialogView.Companion.TAG_DATE_DIALOG
 import kotlinx.android.synthetic.main.fragment_detail_employee.*
 
-class DetailEmployeeFragment : Fragment(R.layout.fragment_detail_employee) {
+class DetailEmployeeFragment : Fragment(){
 
     private var date: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentDetailEmployeeBinding.inflate(inflater)
+            .apply {
+                test = false
+            }.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/sdos/commerce/ui/viewmodels/DetailEmployeeViewModel.kt
@@ -0,0 +1,6 @@
+package com.sdos.commerce.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class DetailEmployeeViewModel: ViewModel() {
+}
