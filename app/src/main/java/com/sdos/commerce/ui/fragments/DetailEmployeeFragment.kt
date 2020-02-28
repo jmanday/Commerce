280fee4c3a84f085f3b48741775bc9edd509c395
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-02-28 11:14:06 +0100

    [feature/detail_employee] ViewModel added.

--- app/src/main/java/com/sdos/commerce/ui/fragments/DetailEmployeeFragment.kt
+++ app/src/main/java/com/sdos/commerce/ui/fragments/DetailEmployeeFragment.kt
@@ -6,11 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 import com.sdos.commerce.R
 import com.sdos.commerce.databinding.FragmentDetailEmployeeBinding
+import com.sdos.commerce.ui.viewmodels.DetailEmployeeViewModel
 import com.sdos.commerce.ui.views.DateDialogView
 import com.sdos.commerce.ui.views.DateDialogView.Companion.TAG_DATE_DIALOG
 import kotlinx.android.synthetic.main.fragment_detail_employee.*
@@ -18,6 +22,7 @@ import kotlinx.android.synthetic.main.fragment_detail_employee.*
 class DetailEmployeeFragment : Fragment(){
 
     private var date: String? = null
+    private lateinit var viewModel: DetailEmployeeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,10 +37,20 @@ class DetailEmployeeFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DetailEmployeeViewModel::class.java)
+        viewModel.init()
         initialize()
     }
 
     private fun initialize() {
+        viewModel.getListSkills().observe(this, Observer {
+            context?.let {context ->
+                val adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, it.map { it.name })
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spn_rol.adapter = adapter
+            }
+        })
+
         ed_date.setOnClickListener {
             fragmentManager?.let { fm ->
                 DateDialogView.newInstance()
--- app/src/main/java/com/sdos/commerce/ui/viewmodels/DetailEmployeeViewModel.kt
+++ app/src/main/java/com/sdos/commerce/ui/viewmodels/DetailEmployeeViewModel.kt
@@ -1,6 +1,29 @@
 package com.sdos.commerce.ui.viewmodels
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import com.sdos.commerce.CommerceApp
+import com.sdos.commerce.domain.injector.DomainInjector
+import com.sdos.commerce.entities.Employee
+import com.sdos.commerce.entities.Skill
 
 class DetailEmployeeViewModel: ViewModel() {
+
+    private val skills = MediatorLiveData<List<Skill>>()
+    private val employees = MediatorLiveData<List<Employee>>()
+
+    val getListSkillInteractor =  (CommerceApp.getInstance() as DomainInjector).provideGetListSkillInteractor()
+    val addEmployeeinteractor =  (CommerceApp.getInstance() as DomainInjector).provideGetListSkillInteractor()
+
+    fun init() {
+        getListSkillInteractor.invoke()?.let {source ->
+            skills.addSource(source, Observer{
+                skills.removeSource(source)
+                skills.value = it
+            })
+        }
+    }
+
+    fun getListSkills() = skills
 }
