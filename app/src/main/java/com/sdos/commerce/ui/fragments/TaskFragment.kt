8ac027a0d5e00035ee55460b0ab9eb10ab18a992
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-03-01 13:00:42 +0100

    [feature/filter_task] Databinding added in main fragment.

--- app/src/main/java/com/sdos/commerce/ui/fragments/TaskFragment.kt
+++ app/src/main/java/com/sdos/commerce/ui/fragments/TaskFragment.kt
@@ -2,18 +2,29 @@ package com.sdos.commerce.ui.fragments
 
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.sdos.commerce.R
+import com.sdos.commerce.databinding.FragmentMainBinding
 import com.sdos.commerce.ui.adapters.TaskAdapter
 import com.sdos.commerce.ui.viewmodels.TaskFragmentViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
 
-class TaskFragment : Fragment(R.layout.fragment_main) {
+class TaskFragment : BaseFragment() {
 
     private lateinit var viewModel: TaskFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentMainBinding.inflate(inflater).root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
