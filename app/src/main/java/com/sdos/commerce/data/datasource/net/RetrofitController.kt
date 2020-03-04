f74468d8143bcabea4be89f64235c588aab2d9ff
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-03-04 11:42:32 +0100

    [feature/retrofit] RetrofitController added in order to have only an instance of retrofit.

---
+++ app/src/main/java/com/sdos/commerce/data/datasource/net/RetrofitController.kt
@@ -0,0 +1,20 @@
+package com.sdos.commerce.data.datasource.net
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitController {
+
+    private const val BASE_URL = "https://data.ct.gov/resource/hma6-9xbg.json/"
+    private var retrofit: Retrofit
+
+    init {
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getInstance() = retrofit
+
+}
