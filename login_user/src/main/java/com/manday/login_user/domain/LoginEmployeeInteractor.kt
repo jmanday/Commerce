cb5d03d85aef54002772dbb96d0f1fcdd2cb8737
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-03-29 17:51:15 +0200

    [bug/COMMERCE-2] LoginEmployeeInteractor moved to "login_user" module.

--- login_user/src/main/java/com/manday/login_user/domain/LoginEmployeeInteractor.kt
+++ login_user/src/main/java/com/manday/login_user/domain/LoginEmployeeInteractor.kt
@@ -1,12 +1,12 @@
 package com.sdos.login.domain
 
 import androidx.lifecycle.LiveData
-import com.sdos.commerce.entities.Employee
-
+/*
 class LoginEmployeeInteractor(private val repository: EmployeeRepository): (String, String) -> LiveData<Employee>? {
 
     override fun invoke(user: String, pass: String): LiveData<Employee>? {
         return repository.login(user, pass)
     }
-
 }
+
+ */
