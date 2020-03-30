bdbdf2333e7936e30ebbad6791a66f5ba0a7371f
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-03-30 18:11:01 +0200

    [feature/COMMERCE-11] Dependency to roomController injected in koin modules.

--- login-user/src/main/java/com/manday/loginuser/di/Modules.kt
+++ login-user/src/main/java/com/manday/loginuser/di/Modules.kt
@@ -1,5 +1,6 @@
 package com.manday.loginuser.di
 
+import com.manday.coredata.controllers.RoomController
 import com.manday.loginuser.data.LoginDataSourceImpl
 import com.manday.loginuser.domain.LoginRepository
 import com.manday.loginuser.domain.LoginRepositoryImpl
@@ -11,12 +12,13 @@ val loginUserModule = module {
 
     // single instance of EmployeeRepository
     single<LoginRepository> {
-        LoginDataSourceImpl()
+        LoginDataSourceImpl(get())
         LoginRepositoryImpl(get())
     }
 
     // Simple Interactor Factory
     factory {
+        RoomController()
         LoginEmployeeInteractor(get())
         LoginDialogViewModel(get())
     }
