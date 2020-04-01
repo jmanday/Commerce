a54f8c06423fff99172596f4961cb49e601ed0a1
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-01 18:16:46 +0200

    [feature/COMMERCE-18] EmployeeDataSource updated.

--- core-data/src/main/java/com/manday/coredata/datasource/EmployeeDataSource.kt
+++ core-data/src/main/java/com/manday/coredata/datasource/EmployeeDataSource.kt
@@ -1,13 +1,12 @@
 package com.manday.coredata.datasource
 
-import androidx.lifecycle.LiveData
 import com.manday.coredata.entities.EmployeeEntity
 
 interface EmployeeDataSource {
 
     fun login(param1: String, param2: String): EmployeeEntity?
 
-    fun getEmployees(): LiveData<List<EmployeeEntity>>?
+    fun getEmployees(): List<EmployeeEntity>?
 
     fun addEmployee(employee: EmployeeEntity)
 }
