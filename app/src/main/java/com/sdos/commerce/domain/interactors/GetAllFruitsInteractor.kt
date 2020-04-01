38f892557279ca4c38925b65e162d610151ffbbd
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-01 11:37:00 +0200

    [feature/COMMERCE-11] Interactors commented.

--- app/src/main/java/com/sdos/commerce/domain/interactors/GetAllFruitsInteractor.kt
+++ app/src/main/java/com/sdos/commerce/domain/interactors/GetAllFruitsInteractor.kt
@@ -1,12 +1,13 @@
 package com.sdos.commerce.domain.interactors
 
 import androidx.lifecycle.LiveData
-import com.sdos.commerce.domain.FruitRepository
-import com.sdos.commerce.entities.Fruit
 
+/*
 class GetAllFruitsInteractor(private val repository: FruitRepository): (String, String) -> LiveData<List<Fruit>> {
 
     override fun invoke(category: String, item: String): LiveData<List<Fruit>> {
         return repository.getAllFruits(category, item)
     }
 }
+
+ */
