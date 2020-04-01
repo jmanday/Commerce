d7eb98456a893cfe17f25441dc4c4cb61af045e4
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-01 11:53:28 +0200

    [feature/COMMERCE-11] File "Modules" for the dependency injection updated.

--- login-user/src/main/java/com/manday/loginuser/di/Modules.kt
+++ login-user/src/main/java/com/manday/loginuser/di/Modules.kt
@@ -1,23 +1,26 @@
 package com.manday.loginuser.di
 
 import com.manday.coredata.controllers.RoomController
+import com.manday.coredata.datasource.EmployeeDataSource
 import com.manday.loginuser.domain.LoginRepository
 import com.manday.loginuser.domain.LoginRepositoryImpl
 import com.manday.loginuser.viewmodels.LoginDialogViewModel
 import com.sdos.login.domain.LoginEmployeeInteractor
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val loginUserModule = module {
 
     // single instance of EmployeeRepository
+    single<EmployeeDataSource> {
+        RoomController()
+    }
     single<LoginRepository> {
         LoginRepositoryImpl(get())
     }
 
     // Simple Interactor Factory
-    factory {
-        RoomController()
-        LoginEmployeeInteractor(get())
-        LoginDialogViewModel(get())
-    }
+    factory { LoginEmployeeInteractor(get()) }
+
+    viewModel { LoginDialogViewModel(get()) }
 }
