8e6bba1c56fa9acb9bf75192fcacc8f715919cbf
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-03 13:57:01 +0200

    [feature/COMMERCE-20] Dependency injection updated.

--- app/src/main/java/com/sdos/commerce/di/Modules.kt
+++ app/src/main/java/com/sdos/commerce/di/Modules.kt
@@ -3,6 +3,7 @@ package com.sdos.commerce.di
 
 import com.manday.coredata.controllers.RoomController
 import com.manday.coredata.datasource.SkillDataSource
+import com.manday.coredata.datasource.TaskDatabaseDataSource
 import com.sdos.commerce.domain.interactors.*
 import com.sdos.commerce.ui.viewmodels.*
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -12,10 +13,11 @@ val appModuleDependencies = module {
 
     // Single instances
     single<SkillDataSource> { RoomController() }
+    single<TaskDatabaseDataSource> { RoomController() }
 
     single<SkillRepository> { SkillRepositoryImpl(get()) }
     single<EmployeeRepository> { EmployeeRepositoryImpl(get()) }
-
+    single<TaskRepository> { TaskRepositoryImpl(get()) }
 
     // ViewModel instances
     viewModel { SplashFragmentViewModel() }
@@ -23,5 +25,5 @@ val appModuleDependencies = module {
     viewModel { DetailEmployeeViewModel(get(), get()) }
     viewModel { DetailTaskViewModel() }
     viewModel { SettingsViewModel() }
-    viewModel { TaskFragmentViewModel() }
+    viewModel { TaskFragmentViewModel(get()) }
 }
