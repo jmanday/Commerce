2862f55a342e3538a86031b3f004b206832abc9e
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-06 16:44:55 +0200

    [feature/COMMERCE-28] Duplicated file removed.

--- core-data/src/main/java/com/manday/coredata/datasource/LoginDatabaseDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.manday.coredata.datasource
-
-import com.manday.coredata.entities.EmployeeEntity
-
-interface LoginDatabaseDataSource {
-
-    fun login(param1: String, param2: String): EmployeeEntity?
-}
