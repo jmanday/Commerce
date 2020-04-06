8683562a260e63fc3c9e5e9ea2b303471be1a25b
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-06 10:21:51 +0200

    [feature/COMMERCE-20] TaskRepository added.

--- app/src/main/java/com/sdos/commerce/domain/interactors/TaskRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.sdos.commerce.domain.interactors
-
-import androidx.lifecycle.LiveData
-import com.manday.coredata.entities.TaskEntity
-
-interface TaskRepository {
-
-    fun getAllTasks(): LiveData<List<TaskEntity>>?
-}
