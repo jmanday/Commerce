cab3b1856d52e7b2e787e6ece0da450c2fb65e44
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-06 10:22:20 +0200

    [feature/COMMERCE-20] SkillRepository added.

--- app/src/main/java/com/sdos/commerce/domain/interactors/SkillRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.sdos.commerce.domain.interactors
-
-import androidx.lifecycle.LiveData
-import com.manday.coredata.entities.SkillEntity
-
-interface SkillRepository {
-
-    fun getListSkill(): LiveData<List<SkillEntity>>?
-}
