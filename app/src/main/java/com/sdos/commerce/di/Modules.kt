79e6afecf5604ad507f62e0acc81427747922ce9
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-06 10:44:12 +0200

    [feature/COMMERCE-20] Modules updated for the appModuleDependencies.

--- app/src/main/java/com/sdos/commerce/di/Modules.kt
+++ app/src/main/java/com/sdos/commerce/di/Modules.kt
@@ -2,8 +2,10 @@ package com.sdos.commerce.di
 
 
 import com.manday.coredata.controllers.RoomController
+import com.manday.coredata.datasource.FruitNetDataSource
 import com.manday.coredata.datasource.SkillDataSource
 import com.manday.coredata.datasource.TaskDatabaseDataSource
+import com.sdos.commerce.data.datasource.net.NetController
 import com.sdos.commerce.domain.interactors.*
 import com.sdos.commerce.ui.viewmodels.*
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,16 +16,18 @@ val appModuleDependencies = module {
     // Single instances
     single<SkillDataSource> { RoomController() }
     single<TaskDatabaseDataSource> { RoomController() }
+    single<FruitNetDataSource> { NetController() }
 
     single<SkillRepository> { SkillRepositoryImpl(get()) }
     single<EmployeeRepository> { EmployeeRepositoryImpl(get()) }
     single<TaskRepository> { TaskRepositoryImpl(get()) }
+    single<FruitRepository> { FruitRepositoryImpl(get()) }
 
     // ViewModel instances
     viewModel { SplashFragmentViewModel() }
     viewModel { EmployeeFragmentViewModel(get()) }
     viewModel { DetailEmployeeViewModel(get(), get()) }
-    viewModel { DetailTaskViewModel() }
-    viewModel { SettingsViewModel() }
+    viewModel { DetailTaskViewModel(get(), get()) }
+    viewModel { SettingsViewModel(get()) }
     viewModel { TaskFragmentViewModel(get()) }
 }
