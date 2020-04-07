27d6952ff41ba7a20dbaa1120f57a231d8a77d47
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-07 17:36:54 +0200

    [feature/COMMERCE-33] FruitViewModel updated.

--- fruit/src/main/java/com/manday/fruit/ui/viewmodel/FruitViewModel.kt
+++ fruit/src/main/java/com/manday/fruit/ui/viewmodel/FruitViewModel.kt
@@ -1,10 +1,20 @@
 package com.manday.fruit.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import com.manday.coredata.ExecutorViewModel
+import com.manday.coredata.entities.FruitEntity
 import com.manday.fruit.repository.FruitRepository
 
 class FruitViewModel(
     private val repository: FruitRepository
 ): ExecutorViewModel() {
 
+    fun getAllFruits(): LiveData<List<FruitEntity>> {
+        return repository.getAllFruits(CATEGORY, ITEM)
+    }
+
+    companion object {
+        const val CATEGORY = "Fruit"
+        const val ITEM = "Peaches"
+    }
 }
