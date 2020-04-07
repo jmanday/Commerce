f6a1a6f1456a176f7ccd138576340f112c63bb0e
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-07 09:12:15 +0200

    [feature/COMMERCE-13] BaseAdapter and BaseViewHolder removed from the module "app".

--- app/src/main/java/com/sdos/commerce/ui/viewholders/BaseViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.sdos.commerce.ui.viewholders
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-open class BaseViewHolder<T>(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    open fun onBind(t: T, f: (t: T) -> Unit) {}
-}
