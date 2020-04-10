c97bac87445796e1fbcb56823857753973892ac4
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-10 11:06:31 +0200

    [feature/COMMERCE-42] ResponseViewModelEntity updated.

--- app/src/main/java/com/sdos/commerce/ResponseViewModelEntity.kt
+++ app/src/main/java/com/sdos/commerce/ResponseViewModelEntity.kt
@@ -1,35 +1,17 @@
 package com.sdos.commerce
 
+
 class ResponseViewModelEntity<T> private constructor(){
 
     var text: String = String()
     var extra: T? = null
-    var extraList: List<T>? = null
-    var typeResponse: TypeResponse = TypeResponse.TEXT
 
     companion object {
 
-        fun<T> createResponse(text: String, extra: T? = null, extraList: List<T>? = null): ResponseViewModelEntity<T> {
-            val response = ResponseViewModelEntity<T>()
-            if (text.isNotEmpty()) {
-                response.text = text
+        fun<T> createResponse(text: String = String(), extra: T? = null) =
+            ResponseViewModelEntity<T>().apply {
+                this.text = text
+                this.extra = extra
             }
-
-            if (extra != null) {
-                response.extra = extra
-                response.typeResponse = TypeResponse.EXTRA
-            }
-
-            if (!extraList.isNullOrEmpty()) {
-                response.extraList = extraList
-                response.typeResponse = TypeResponse.EXTRA_LIST
-            }
-
-            return response
-        }
     }
-}
-
-enum class TypeResponse {
-    TEXT, EXTRA, EXTRA_LIST, TEXT_EXTRA
 }
