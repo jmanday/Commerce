bc5d7517907109683c24ed92eafc527a7bf8daef
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-10 11:07:09 +0200

    [feature/COMMERCE-42] Method in order to return a LiveData when the source LiveData changes added in Utils.

--- app/src/main/java/com/sdos/commerce/util/Utils.kt
+++ app/src/main/java/com/sdos/commerce/util/Utils.kt
@@ -2,9 +2,23 @@ package com.sdos.commerce.util
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import java.util.*
 
 fun String.toBitmap(): Bitmap {
     val imageBytes = Base64.getMimeDecoder().decode(this)
     return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+}
+
+fun<T, U> transformWhenItChanges(source: LiveData<T>?, f: (T) -> U): LiveData<U> {
+    val mediatorLiveData = MediatorLiveData<U>()
+
+    source?.let {
+        it.observeForever {
+            mediatorLiveData.value = f(it)
+        }
+    }
+
+    return mediatorLiveData
 }
