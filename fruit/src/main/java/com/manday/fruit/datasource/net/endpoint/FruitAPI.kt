899beaa3ee08729d37a36e836c3b1ea2f7d9ed4d
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-12 19:01:46 +0200

    [feature/COMMERCE-56] FruitEntity moved to core fruit.

--- fruit/src/main/java/com/manday/fruit/datasource/net/endpoint/FruitAPI.kt
+++ fruit/src/main/java/com/manday/fruit/datasource/net/endpoint/FruitAPI.kt
@@ -1,6 +1,6 @@
 package com.manday.fruit.datasource.net.endpoint
 
-import com.manday.coredata.entities.FruitModel
+import com.manday.fruit.entities.FruitEntity
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
@@ -9,5 +9,5 @@ import retrofit2.http.QueryMap
 interface FruitAPI {
 
     @GET("hma6-9xbg.json/")
-    fun getAllFruits(@QueryMap params: Map<String, String>): Call<List<FruitModel>>
+    fun getAllFruits(@QueryMap params: Map<String, String>): Call<List<FruitEntity>>
 }
--- fruit/src/main/java/com/manday/fruit/ui/viewholder/FruitViewHolder.kt
+++ fruit/src/main/java/com/manday/fruit/ui/viewholder/FruitViewHolder.kt
@@ -1,13 +1,13 @@
 package com.manday.fruit.ui.viewholder
 
 import android.view.View
-import com.manday.coredata.entities.FruitEntity
 import com.manday.coreui.viewholder.BaseViewHolder
+import com.manday.fruit.models.FruitModel
 import kotlinx.android.synthetic.main.list_fruit_item_view.view.*
 
-internal class FruitViewHolder(itemView: View): BaseViewHolder<FruitEntity>(itemView) {
+internal class FruitViewHolder(itemView: View): BaseViewHolder<FruitModel>(itemView) {
 
-    override fun onBind(t: FruitEntity, f: (t: FruitEntity) -> Unit) {
+    override fun onBind(t: FruitModel, f: (t: FruitModel) -> Unit) {
         itemView.name.text = t.farmName
         itemView.category.text = t.category
         itemView.businness.text = t.business
