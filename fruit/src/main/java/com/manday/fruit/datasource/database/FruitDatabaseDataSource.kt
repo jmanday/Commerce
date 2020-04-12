e669fb657d40f44f6c44c972d5588040fb9551c7
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-12 19:04:41 +0200

    [feature/COMMERCE-56] New method added in FruitDatabaseDataSource.

--- fruit/src/main/java/com/manday/fruit/datasource/database/FruitDatabaseDataSource.kt
+++ fruit/src/main/java/com/manday/fruit/datasource/database/FruitDatabaseDataSource.kt
@@ -1,8 +1,11 @@
 package com.manday.fruit.datasource.database
 
+import androidx.lifecycle.LiveData
 import com.manday.coredata.entities.FruitEntity
 
 interface FruitDatabaseDataSource {
 
     fun addFruits(list: List<FruitEntity>)
+
+    fun getAllLocalFruits(): LiveData<List<FruitEntity>>
 }
