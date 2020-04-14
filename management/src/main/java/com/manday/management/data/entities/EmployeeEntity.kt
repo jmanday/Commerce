af21667adf84ce54cec7de7868b39c876d7b38d2
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-14 12:40:56 +0200

    [feature/COMMERCE-61] EmployeeEntity added to new module management.

---
+++ management/src/main/java/com/manday/management/data/entities/EmployeeEntity.kt
@@ -0,0 +1,49 @@
+package com.manday.management.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "employees")
+data class EmployeeEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+
+    @ColumnInfo(name = "name")
+    var name: String = String(),
+
+    @ColumnInfo(name = "surname")
+    var surname: String = String(),
+
+    @ColumnInfo(name = "email")
+    var email: String = String(),
+
+    @ColumnInfo(name = "typeEmployee")
+    var typeEmployee: Int = 1,
+
+    @ColumnInfo(name = "phone")
+    var phone: String = String(),
+
+    @ColumnInfo(name = "pass")
+    var pass:  String = String(),
+
+    @ColumnInfo(name = "hours")
+    var hours: Double = 0.0,
+
+    @ColumnInfo(name = "skill")
+    var skill: Int = 0,
+
+    @ColumnInfo(name = "listTask")
+    var listTask: List<Int> = listOf(),
+
+    @ColumnInfo(name = "currenTask")
+    var currenTask: Int = 0,
+
+    @ColumnInfo(name = "image")
+    var image: String = String(),
+
+    @ColumnInfo(name = "birthdate")
+    var birthdate: String = String()
+): Serializable
