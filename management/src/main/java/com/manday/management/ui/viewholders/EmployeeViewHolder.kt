144abdee7b13ebbd55cdb3c4103f9904af7f8322
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-14 16:58:43 +0200

    [feature/COMMERCE-61] ViewHolder and Adapter moved to module management.

---
+++ management/src/main/java/com/manday/management/ui/viewholders/EmployeeViewHolder.kt
@@ -0,0 +1,27 @@
+package com.manday.employee.ui.viewholders
+
+import android.view.View
+import com.bumptech.glide.Glide
+import com.manday.coreui.viewholder.BaseViewHolder
+import com.manday.employee.R
+import kotlinx.android.synthetic.main.list_item_view.view.*
+
+internal class EmployeeViewHolder(itemView: View): BaseViewHolder<EmployeeEntity>(itemView) {
+
+    override fun onBind(employee: EmployeeEntity, f: (employee: EmployeeEntity) -> Unit) {
+        itemView.name.text = employee.name
+        itemView.surname.text = employee.surname
+        itemView.state.text = if (employee.currenTask == -1) itemView.context.resources.getString(R.string.text_state_free)
+                            else String.format(itemView.context.resources.getString(R.string.text_state_busy),employee.currenTask.toString())
+        itemView.text_type.visibility = View.VISIBLE
+        Glide.with(itemView)
+            .load(employee.image)
+            .centerCrop()
+            .placeholder(R.mipmap.placeholder)
+            .into(itemView.img)
+
+        itemView.setOnClickListener {
+            f(employee)
+        }
+    }
+}
