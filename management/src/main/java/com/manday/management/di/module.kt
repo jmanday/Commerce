1576996c5de7d9ab2dcc616b8db50f547b8bc46f
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-14 12:40:32 +0200

    [feature/COMMERCE-61] File module for the di added in the new module.

---
+++ management/src/main/java/com/manday/management/di/module.kt
@@ -0,0 +1,21 @@
+package com.manday.management.di
+
+import com.manday.loginuser.datasource.LoginDatabaseDataSource
+import com.manday.loginuser.datasource.LoginDatabaseDataSourceImpl
+import com.manday.loginuser.repository.LoginRepository
+import com.manday.loginuser.repository.LoginRepositoryImpl
+import com.manday.loginuser.viewmodels.LoginDialogViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val managementModuleDependencies = module {
+
+    // Single instances
+    single<LoginDatabaseDataSource> { LoginDatabaseDataSourceImpl() }
+
+    single<LoginRepository> { LoginRepositoryImpl(get()) }
+
+
+    // ViewModel instances
+    viewModel { LoginDialogViewModel(get()) }
+}
