6e195b542f671cc306d411a4e66332b7e42227aa
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-15 12:23:21 +0200

    [feature/COMMERCE-61] LoginRepository and LoginDatasource removed.

--- management/src/main/java/com/manday/management/data/datasource/LoginDatabaseDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.manday.loginuser.datasource
-
-import androidx.lifecycle.LiveData
-import com.manday.management.data.entities.EmployeeEntity
-
-internal interface LoginDatabaseDataSource {
-
-    fun login(param1: String, param2: String): LiveData<EmployeeEntity>?
-}
--- management/src/main/java/com/manday/management/repository/LoginRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.manday.loginuser.repository
-
-import androidx.lifecycle.LiveData
-import com.manday.management.domain.Employee
-
-internal interface LoginRepository {
-
-    fun login(user: String, pass: String): LiveData<Employee>
-}
