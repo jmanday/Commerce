30c4420e10a4bacde512a6ebde968d3816365ced
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-15 16:48:59 +0200

    [feature/COMMERCE-61] Listener in the view added for the FruitViewHolder.

--- fruit/src/main/java/com/manday/fruit/ui/viewholder/FruitViewHolder.kt
+++ fruit/src/main/java/com/manday/fruit/ui/viewholder/FruitViewHolder.kt
@@ -7,9 +7,13 @@ import kotlinx.android.synthetic.main.list_fruit_item_view.view.*
 
 internal class FruitViewHolder(itemView: View): BaseViewHolder<FruitModel>(itemView) {
 
-    override fun onBind(t: FruitModel, f: (t: FruitModel) -> Unit) {
-        itemView.name.text = t.farmName
-        itemView.category.text = t.category
-        itemView.businness.text = t.business
+    override fun onBind(fruitModel: FruitModel, f: (t: FruitModel) -> Unit) {
+        itemView.name.text = fruitModel.farmName
+        itemView.category.text = fruitModel.category
+        itemView.businness.text = fruitModel.business
+
+        itemView.setOnClickListener {
+            f(fruitModel)
+        }
     }
 }
