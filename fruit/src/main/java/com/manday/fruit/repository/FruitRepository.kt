085827996bdeedc8145af00b54e660d6d8ddd880
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-15 16:49:28 +0200

    [feature/COMMERCE-61] Fruit repository updated with the new logic.

--- fruit/src/main/java/com/manday/fruit/repository/FruitRepository.kt
+++ fruit/src/main/java/com/manday/fruit/repository/FruitRepository.kt
@@ -2,10 +2,11 @@ package com.manday.fruit.repository
 
 import androidx.lifecycle.LiveData
 import com.manday.fruit.entities.FruitEntity
+import com.manday.fruit.models.FruitModel
 
 interface FruitRepository {
 
-    fun getAllFruits(category: String, item: String): LiveData<List<FruitEntity>>?
+    fun getAllFruits(category: String, item: String): LiveData<List<FruitModel>?>?
 
     //fun getLocalAllFruits(): LiveData<List<FruitEntity>>
 
--- fruit/src/main/java/com/manday/fruit/repository/FruitRepositoryImpl.kt
+++ fruit/src/main/java/com/manday/fruit/repository/FruitRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.manday.fruit.repository
 
-import androidx.lifecycle.LiveData
-import com.manday.fruit.entities.FruitEntity
+import com.manday.coredata.utils.transformMapResponse
 import com.manday.fruit.datasource.net.FruitNetDataSource
 
 class FruitRepositoryImpl(
     private val dataSource: FruitNetDataSource
 ): FruitRepository {
 
-    override fun getAllFruits(category: String, item: String): LiveData<List<FruitEntity>>? {
-        return dataSource.getFruits(category, item)
-    }
+    override fun getAllFruits(category: String, item: String) =
+        transformMapResponse(dataSource.getFruits(category, item)){
+            it.map { it.mapToFruitModel() }
+        }
 }
