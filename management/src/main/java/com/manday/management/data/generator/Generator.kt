6de00fb67f060008f2ad92ad35e5a31ce40e7361
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-15 16:45:02 +0200

    [feature/COMMERCE-61] Generator updated.

--- management/src/main/java/com/manday/management/data/generator/Generator.kt
+++ management/src/main/java/com/manday/management/data/generator/Generator.kt
@@ -7,7 +7,8 @@ object Generator {
     fun getEmployees() = listOf(
         EmployeeEntity(null, "Ruben", "García", "amolino@gmail.com",0, "677889933", "1234", 0.0, 0, listOf(), -1, "https://idatosabiertos.org/wp-content/uploads/2017/04/juanpane.png", "12/3/77"),
         EmployeeEntity(null, "Rafael", "Martín", "rmartino@gmail.com",1, "677889933", "1234", 0.0, 2, listOf(), -1, "https://upload.wikimedia.org/wikipedia/commons/b/b7/Juan_Darth%C3%A9s_.png", "24/11/80"),
-        EmployeeEntity(null, "Sara", "López", "sploez@gmail.com",1, "677889933", "1234", 0.0, 1, listOf(), -1, "https://etfsantpau.com/wp-content/uploads/2019/03/Docentes_v2_0017_Juan-Luis-Linares-1.jpg", "2/8/90")
+        EmployeeEntity(null, "María", "Robles", "mrobles@gmail.com",1, "677889933", "1234", 0.0, 2, listOf(), -1, "https://administracion.escueladeejecutivos.org/uploadedFiles/7221_Profe_Ana_foto.JPG", "24/11/80"),
+        EmployeeEntity(null, "Ramón", "López", "sploez@gmail.com",1, "677889933", "1234", 0.0, 1, listOf(), -1, "https://etfsantpau.com/wp-content/uploads/2019/03/Docentes_v2_0017_Juan-Luis-Linares-1.jpg", "2/8/90")
     )
 
     fun getSkills() = listOf(
