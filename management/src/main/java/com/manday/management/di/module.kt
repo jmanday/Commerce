771cbbbe85567cd0ee5012cb110a5d70818f2e4b
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-17 16:27:30 +0200

    [feature/COMMERCE-64] Files module for the di updated and created for the management module.

--- management/src/main/java/com/manday/management/di/module.kt
+++ management/src/main/java/com/manday/management/di/module.kt
@@ -1,11 +1,18 @@
 package com.manday.management.di
 
+import com.manday.coredata.datasource.SkillDatabaseDataSource
+import com.manday.coredata.datasource.SkillDatabaseDataSourceImpl
+import com.manday.coredata.datasource.TaskDatabaseDataSource
+import com.manday.coredata.datasource.TaskDatabaseDataSourceImpl
 import com.manday.loginuser.viewmodels.LoginDialogViewModel
 import com.manday.management.data.datasource.EmployeeDatabaseDataSource
 import com.manday.management.data.datasource.EmployeeDatabaseDataSourceImpl
+import com.manday.management.repository.*
 import com.manday.management.repository.EmployeeRepository
 import com.manday.management.repository.EmployeeRepositoryImpl
 import com.manday.management.ui.viewmodels.EmployeeViewModel
+import com.manday.management.ui.viewmodels.TaskDetailViewModel
+import com.manday.management.ui.viewmodels.TaskViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,22 +20,17 @@ val managementModuleDependencies = module {
 
     // Single instances
     single<EmployeeDatabaseDataSource> { EmployeeDatabaseDataSourceImpl() }
-
-    single<EmployeeRepository> { EmployeeRepositoryImpl(get()) }
-
-
-    // ViewModel instances
-    viewModel { LoginDialogViewModel(get()) }
-    viewModel { EmployeeViewModel(get()) }
-
     single<SkillDatabaseDataSource> { SkillDatabaseDataSourceImpl() }
     single<TaskDatabaseDataSource> { TaskDatabaseDataSourceImpl() }
 
+    single<EmployeeRepository> { EmployeeRepositoryImpl(get()) }
     single<SkillRepository> { SkillRepositoryImpl(get()) }
     single<TaskRepository> { TaskRepositoryImpl(get()) }
 
     // ViewModel instances
-    viewModel { SplashViewModel() }
-    viewModel { DetailTaskViewModel(get()) }
+    viewModel { LoginDialogViewModel(get()) }
+    viewModel { EmployeeViewModel(get()) }
+    viewModel { TaskDetailViewModel(get()) }
     viewModel { TaskViewModel(get()) }
+
 }
