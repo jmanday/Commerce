0745be95673c93c6541daa7ab0f524f5e352e344
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-18 13:51:26 +0200

    [feature/COMMERCE-64] Adapters and ViewHolders update for pass the itemView as well.

--- management/src/main/java/com/manday/management/ui/adapters/TaskAdapter.kt
+++ management/src/main/java/com/manday/management/ui/adapters/TaskAdapter.kt
@@ -1,13 +1,14 @@
 package com.manday.management.ui.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.manday.management.data.entities.TaskEntity
 import com.manday.coreui.adapter.BaseAdapter
 import com.manday.management.R
 import com.manday.management.ui.viewholders.TaskViewHolder
 
-class TaskAdapter(var listTask: List<TaskEntity>, var listener: (TaskEntity) -> Unit): BaseAdapter<TaskEntity, TaskViewHolder>(listTask, listener) {
+class TaskAdapter(var listTask: List<TaskEntity>, var listener: (TaskEntity, View) -> Unit): BaseAdapter<TaskEntity, TaskViewHolder>(listTask, listener) {
 
     override fun generateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_view, parent, false)
--- management/src/main/java/com/manday/management/ui/viewholders/TaskViewHolder.kt
+++ management/src/main/java/com/manday/management/ui/viewholders/TaskViewHolder.kt
@@ -9,13 +9,13 @@ import kotlinx.android.synthetic.main.list_item_employee_view.view.*
 
 class TaskViewHolder(itemView: View): BaseViewHolder<TaskEntity>(itemView) {
 
-    override fun onBind(task: TaskEntity, f: (task: TaskEntity) -> Unit) {
+    override fun onBind(task: TaskEntity, f: (task: TaskEntity, v: View) -> Unit) {
         itemView.name.text = task.name
         itemView.state.text = String.format(task.duration.toString(), " horas")
         itemView.text_type.visibility = View.VISIBLE
         itemView.text_type.text = "Duración"
         itemView.setOnClickListener {
-            f(task)
+            f(task, itemView)
         }
         Glide.with(itemView)
             .load(task.image)
@@ -24,7 +24,7 @@ class TaskViewHolder(itemView: View): BaseViewHolder<TaskEntity>(itemView) {
             .into(itemView.img)
 
         itemView.setOnClickListener {
-            f(task)
+            f(task, itemView)
         }
     }
 }
