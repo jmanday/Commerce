0dd7a7627afaaa6114734b2b018decb7441a0452
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-20 17:27:18 +0200

    [feature/COMMERCE-64] EmployeeViewModel updated with skillRepository.

--- management/src/main/java/com/manday/management/ui/viewmodels/EmployeeDetailViewModel.kt
+++ management/src/main/java/com/manday/management/ui/viewmodels/EmployeeDetailViewModel.kt
@@ -1,23 +1,42 @@
 package com.manday.management.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
 import com.manday.coredata.ExecutorViewModel
 import com.manday.coredata.HandlerResponseViewModel
+import com.manday.coredata.utils.addSourceNotNull
+import com.manday.coredata.utils.removeSourceNotNull
 import com.manday.management.data.entities.EmployeeEntity
+import com.manday.management.data.entities.SkillEntity
 import com.manday.management.domain.EmployeeModel
 import com.manday.management.isDateValidate
 import com.manday.management.repository.EmployeeRepository
+import com.manday.management.repository.SkillRepository
 
 internal class EmployeeDetailViewModel(
-    private val employeeRepository: EmployeeRepository
+    private val employeeRepository: EmployeeRepository,
+    private val skillRepository: SkillRepository
 ): ExecutorViewModel() {
 
     private val errorFieldList = mutableListOf<ErrorField>()
     private lateinit var responseViewModelEntity: HandlerResponseViewModel<List<ErrorField>>
     var employeeModel: EmployeeModel? = null
+    private var skills = MediatorLiveData<List<SkillEntity>>()
 
     fun initialize(employee: EmployeeModel) {
         this.employeeModel = employee
     }
+
+    fun skills(): LiveData<List<SkillEntity>> {
+        skills.addSourceNotNull(skillRepository.getListSkill(), Observer {
+            skills.removeSourceNotNull(skillRepository.getListSkill())
+            skills.postValue(it)
+        })
+
+        return skills
+    }
+
 
     fun onButtonAddClicked(employee: EmployeeModel): HandlerResponseViewModel<List<ErrorField>> {
         employee.let {
