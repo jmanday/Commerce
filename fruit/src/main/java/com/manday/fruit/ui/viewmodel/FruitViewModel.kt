cf536eed43c1166d3f0eba0d11eaa4df50edcc85
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-22 14:11:29 +0200

    [feature/COMMERCE-65] More refactor.

--- fruit/src/main/java/com/manday/fruit/ui/viewmodel/FruitViewModel.kt
+++ fruit/src/main/java/com/manday/fruit/ui/viewmodel/FruitViewModel.kt
@@ -1,21 +1,37 @@
 package com.manday.fruit.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
 import com.manday.coredata.ExecutorViewModel
-import com.manday.coredata.HandlerResponseViewModel
-import com.manday.coredata.utils.transformMap
-import com.manday.coredata.utils.transformWhenItChanges
+import com.manday.coredata.utils.addSourceNotNull
+import com.manday.coredata.utils.removeSourceNotNull
+import com.manday.fruit.models.FruitModel
 import com.manday.fruit.repository.FruitRepository
 
 class FruitViewModel(
     private val repository: FruitRepository
 ): ExecutorViewModel() {
 
-    fun getAllFruits() =
-        HandlerResponseViewModel.createResponse(repository.getAllFruits(CATEGORY, ITEM))
+    private val fruitsList = MediatorLiveData<List<FruitModel>?>()
+
+    fun fruits(): LiveData<List<FruitModel>?> {
+        fruitsList.addSourceNotNull(repository.getAllFruits(CATEGORY, ITEM), Observer<List<FruitModel>?> {
+            fruitsList.removeSourceNotNull(repository.getAllFruits(CATEGORY, ITEM))
+            fruitsList.postValue(it)
+        })
+
+        return fruitsList
+    }
 
 
     companion object {
         const val CATEGORY = "Fruit"
         const val ITEM = "Peaches"
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        val a = 22
     }
 }
