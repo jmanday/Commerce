6f91245bfdb333e31eb4d0311f88f24d19463fe6
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-30 09:21:41 +0200

    [feature/COMMERCE-53] Minor changes.

--- fruit/src/main/java/com/manday/fruit/datasource/net/FruitNetDataSourceImpl.kt
+++ fruit/src/main/java/com/manday/fruit/datasource/net/FruitNetDataSourceImpl.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.manday.coredata.controllers.retrofit.RetrofitController
-import com.manday.fruit.entities.FruitEntity
 import com.manday.fruit.datasource.net.endpoint.FruitAPI
+import com.manday.fruit.entities.FruitEntity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,8 +13,8 @@ import retrofit2.Response
 class FruitNetDataSourceImpl:
     FruitNetDataSource {
 
-    override fun getFruits(category: String, item: String): LiveData<List<FruitEntity>>? {
-        val data = MutableLiveData<List<FruitEntity>>()
+    override fun getFruits(category: String, item: String): LiveData<List<FruitEntity>?> {
+        val data = MutableLiveData<List<FruitEntity>?>()
         val service = RetrofitController.createRequest<FruitAPI>()
 
         val call = service.getAllFruits(hashMapOf(
@@ -25,6 +25,7 @@ class FruitNetDataSourceImpl:
         call.enqueue(object : Callback<List<FruitEntity>> {
             override fun onFailure(call: Call<List<FruitEntity>>, t: Throwable) {
                 Log.d("Retrofit Error", t.message)
+                data.value = null
             }
 
             override fun onResponse(call: Call<List<FruitEntity>>, response: Response<List<FruitEntity>>) {
--- fruit/src/main/java/com/manday/fruit/repository/FruitRepository.kt
+++ fruit/src/main/java/com/manday/fruit/repository/FruitRepository.kt
@@ -1,12 +1,11 @@
 package com.manday.fruit.repository
 
 import androidx.lifecycle.LiveData
-import com.manday.fruit.entities.FruitEntity
 import com.manday.fruit.models.FruitModel
 
 interface FruitRepository {
 
-    fun getAllFruits(category: String, item: String): LiveData<List<FruitModel>?>?
+    fun getAllFruits(category: String, item: String): LiveData<List<FruitModel>?>
 
     //fun getLocalAllFruits(): LiveData<List<FruitEntity>>
 
