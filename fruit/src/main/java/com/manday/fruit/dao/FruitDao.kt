a7d06da7045f8f519c35f86006f4ec7ca23eb896
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-04-30 09:16:36 +0200

    [feature/COMMERCE-53] FruitDao added.

--- fruit/src/main/java/com/manday/fruit/dao/FruitDao.kt
+++ fruit/src/main/java/com/manday/fruit/dao/FruitDao.kt
@@ -1,11 +1,11 @@
-package com.manday.management.data.dao
+package com.manday.fruit.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.manday.management.data.entities.FruitEntity
+import com.manday.fruit.entities.FruitEntity
 
 @Dao
 interface FruitDao {
@@ -14,5 +14,5 @@ interface FruitDao {
     fun insert(fruits: List<FruitEntity>)
 
     @Query("SELECT * FROM fruits")
-    fun getAllFruitss(): LiveData<List<FruitEntity>>
+    fun getAllFruits(): LiveData<List<FruitEntity>?>
 }
