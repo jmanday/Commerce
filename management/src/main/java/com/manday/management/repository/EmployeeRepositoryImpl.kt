ef650ecb588065a141b368d3900fd3e8a7582f93
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-02 20:06:14 +0200

    [feature/COMMERCE-84] Rename.

--- management/src/main/java/com/manday/management/repository/EmployeeRepositoryImpl.kt
+++ management/src/main/java/com/manday/management/repository/EmployeeRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.manday.management.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.manday.coredata.utils.transformMapResponse
 import com.manday.coredata.utils.transformationsNotNull
 import com.manday.management.data.datasource.EmployeeDatabaseDataSource
@@ -10,25 +8,25 @@ import com.manday.management.data.entities.toEmployee
 import com.manday.management.domain.EmployeeModel
 import com.manday.management.domain.toEmployeeEntity
 
-internal class EmployeeRepositoryImpl(private val dataSource: EmployeeDatabaseDataSource):
+internal class EmployeeRepositoryImpl(private val databseDataSource: EmployeeDatabaseDataSource) :
     EmployeeRepository {
 
     override fun login(user: String, pass: String) =
-        transformMapResponse(dataSource.login(user, pass)) {
+        transformMapResponse(databseDataSource.login(user, pass)) {
             it?.toEmployee()
         }
 
     override fun getEmployees() =
-        transformationsNotNull(dataSource.getEmployees()) {
+        transformationsNotNull(databseDataSource.getEmployees()) {
             it?.map { it.toEmployee() }
         }
 
 
     override fun addEmployee(employeeModel: EmployeeModel) {
-        dataSource.addEmployee(employeeModel.toEmployeeEntity())
+        databseDataSource.addEmployee(employeeModel.toEmployeeEntity())
     }
 
     override fun updateEmployee(employeeEntity: EmployeeEntity?) {
-        dataSource.updateEmployee(employeeEntity)
+        databseDataSource.updateEmployee(employeeEntity)
     }
 }
