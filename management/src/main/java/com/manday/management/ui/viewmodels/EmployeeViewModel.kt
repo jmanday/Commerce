0f6ceb44af651ff9c963d861b53bd8f00cedccca
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-04 11:40:51 +0200

    [feature/COMMERCE-82] EmployeeViewModel updated with new function executor.

--- management/src/main/java/com/manday/management/ui/viewmodels/EmployeeViewModel.kt
+++ management/src/main/java/com/manday/management/ui/viewmodels/EmployeeViewModel.kt
@@ -1,29 +1,36 @@
 package com.manday.management.ui.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.manday.coredata.ExecutorViewModel
-import com.manday.coredata.utils.addMultipleSourceNotNull
 import com.manday.management.domain.EmployeeModel
 import com.manday.management.repository.EmployeeRepository
 import com.manday.management.repository.SkillRepository
+
 
 internal class EmployeeViewModel(
     employeeRepository: EmployeeRepository,
     skillRepository: SkillRepository
 ) : ExecutorViewModel() {
 
-    val employees = MutableLiveData<List<EmployeeModel>?>()
+    var employees: LiveData<List<EmployeeModel>?>
 
     init {
-        addMultipleSourceNotNull(employeeRepository.getEmployees(), skillRepository.getListSkill()) { listEmployees, listSkills ->
-            listEmployees?.map {
-                it.skillEmployeeDescription = listSkills?.find { skill ->
-                    skill.id == it.skillEmployee
-                }?.name ?: ""
+        employees = doBothInBackgroundAndMap(
+            {
+                employeeRepository.getEmployees()
+            }, {
+                skillRepository.getListSkill()
+            },
+            { listEmployees, listSkills ->
+                listEmployees?.map {
+                    it.skillEmployeeDescription = listSkills?.find { skill ->
+                        skill.id == it.skillEmployee
+                    }?.name ?: ""
+                }
+
+                listEmployees
             }
-            employees.postValue(listEmployees)
-        }
+        )
     }
 
 }
