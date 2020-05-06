6bfd3a749378a3b4286473642893121be0b86a17
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-06 10:23:49 +0200

    [feature/COMMERCE-82] Method getAllFruits on the Repository updated.

--- fruit/src/main/java/com/manday/fruit/repository/FruitRepositoryImpl.kt
+++ fruit/src/main/java/com/manday/fruit/repository/FruitRepositoryImpl.kt
@@ -1,40 +1,24 @@
 package com.manday.fruit.repository
 
-import androidx.lifecycle.LiveData
 import com.manday.coredata.utils.addMultipleDataSource
 import com.manday.fruit.datasource.database.FruitDatabaseDataSource
 import com.manday.fruit.datasource.net.FruitNetDataSource
-import com.manday.fruit.entities.mapToFruitModel
-import com.manday.fruit.models.FruitModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.manday.fruit.entities.mapToListFruitModel
 
 class FruitRepositoryImpl(
     private val netDataSource: FruitNetDataSource,
     private val databaseDataSource: FruitDatabaseDataSource
 ): FruitRepository {
 
-    private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
-
-    override fun getAllFruits(category: String, item: String): LiveData<List<FruitModel>?> {
-        val listFruitModel = mutableListOf<FruitModel>()
-
-        return addMultipleDataSource(
+    override fun getAllFruits(category: String, item: String) =
+        addMultipleDataSource(
             netDataSource.getFruits(category, item),
             databaseDataSource.getAllLocalFruits(),
             {
-                scope.launch {
-                    databaseDataSource.addFruits(it)
-                }
+                databaseDataSource.addFruits(it)
             },
             {
-                it.forEach {
-                    if (it.isEnabled())
-                        listFruitModel.add(it.mapToFruitModel())
-                }
-
-                listFruitModel.toList()
-            })
-    }
+                it?.mapToListFruitModel()
+            }
+        )
 }
