fa5f1cc0dc7fc33fede947a76c6b173d182da73c
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-12 16:27:43 +0200

    [feature/COMMERCE-86] Refactor in both container transforms.

--- core-ui/src/main/java/com/manday/coreui/transitions/ContainerTransformFit.kt
+++ core-ui/src/main/java/com/manday/coreui/transitions/ContainerTransformFit.kt
@@ -1,15 +1,16 @@
 package com.manday.coredata.transitions
 
 import android.content.Context
+import androidx.core.content.ContextCompat
 import com.google.android.material.transition.MaterialContainerTransform
-import com.manday.coreui.transitions.ContainerTransformData
-import com.manday.coreui.transitions.TransitionData
+import com.manday.coreui.transitions.TransitionAttributes
 
 class ContainerTransformFit : TransitionMode {
 
-    override fun make(context: Context, data: TransitionData) =
+    override fun make(context: Context, attributes: TransitionAttributes) =
         MaterialContainerTransform(context).apply {
-            fitMode = (data as ContainerTransformData).modeTransition
-            duration = 500
+            fitMode = attributes.mode
+            duration = 800
+            scrimColor = ContextCompat.getColor(context, attributes.colorScrim)
         }
 }
