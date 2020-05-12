6cc1ba13afc9e9d930ba2461e16b5fbdb58a00bb
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-12 10:39:52 +0200

    [feature/COMMERCE-86] New dependency added in the module di.

--- management/src/main/java/com/manday/management/di/module.kt
+++ management/src/main/java/com/manday/management/di/module.kt
@@ -7,6 +7,8 @@ import com.manday.coredata.datasource.TaskRoomDataSource
 import com.manday.coredata.transitions.ContainerTransformFade
 import com.manday.management.data.datasource.EmployeeDatabaseDataSource
 import com.manday.management.data.datasource.EmployeeRoomDataSource
+import com.manday.management.navigation.NavigateFromDetailEmployeeToTaskFragment
+import com.manday.management.navigation.NavigateFromEmployeeToDetailFragment
 import com.manday.management.repository.*
 import org.koin.dsl.module
 
@@ -22,5 +24,6 @@ val managementModuleDependencies = module {
     single<TaskRepository> { TaskRepositoryImpl(get()) }
 
     factory { ContainerTransformFade() }
+    factory { NavigateFromEmployeeToDetailFragment() }
 
 }
