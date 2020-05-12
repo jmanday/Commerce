b7d58b901b25667be4ae312cdc909fb234e2d3bf
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-12 16:26:28 +0200

    [feature/COMMERCE-86] NavigateFromEmployeeToDetailFragment with NavArgs.

--- management/src/main/java/com/manday/management/navigation/NavigateFromEmployeeToDetailFragment.kt
+++ management/src/main/java/com/manday/management/navigation/NavigateFromEmployeeToDetailFragment.kt
@@ -1,42 +1,35 @@
 package com.manday.management.navigation
 
-import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.manday.coredata.navigation.Navigate
 import com.manday.coredata.navigation.Navigate.Companion.navController
-import com.manday.management.Constants
-import com.manday.management.Constants.ARGUMENT_EXTRA_NAME_TRANSITION
 import com.manday.management.Constants.NAME_GENERAL_TRANSITION
-import com.manday.management.R
 import com.manday.management.domain.EmployeeModel
+import com.manday.management.ui.fragments.EmployeeFragmentDirections
 
 open class NavigateFromEmployeeToDetailFragment : Navigate<EmployeeModel> {
 
-
-    open val actionId = R.id.action_mainFragment_to_detailEmployeeFragment
-
-
     override fun navigate(itemView: View?, t: EmployeeModel?) {
         var localTransitionName = NAME_GENERAL_TRANSITION
-        var extras: FragmentNavigator.Extras? = null
+        var extras: FragmentNavigator.Extras = FragmentNavigatorExtras()
 
-        val bundle = Bundle().apply {
-            t?.let {
-                putSerializable(Constants.ARGUMENT_EXTRA_EMPLOYEE, it)
-                localTransitionName = it.name
-            }
-
-            itemView?.let {
-                it.transitionName = localTransitionName
-                putString(ARGUMENT_EXTRA_NAME_TRANSITION, it.transitionName)
-                extras = FragmentNavigatorExtras(it to it.transitionName)
-            }
-
+        if (t != null) {
+            localTransitionName = t.name
         }
 
-        navController?.navigate(actionId, bundle, null, extras)
+        if (itemView != null) {
+            itemView.transitionName = localTransitionName
+            extras = FragmentNavigatorExtras(itemView to itemView.transitionName)
+        }
+
+        val action = EmployeeFragmentDirections.actionMainFragmentToDetailEmployeeFragment(
+            t,
+            localTransitionName
+        )
+
+        navController?.navigate(action, extras)
     }
 
 }
