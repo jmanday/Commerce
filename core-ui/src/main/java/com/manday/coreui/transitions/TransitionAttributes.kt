07929be5c4cb6a760738bb8ffc75cc3cc57fd358
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-12 16:25:31 +0200

    [feature/COMMERCE-86] New classes for the transition added.

---
+++ core-ui/src/main/java/com/manday/coreui/transitions/TransitionAttributes.kt
@@ -0,0 +1,8 @@
+package com.manday.coreui.transitions
+
+import com.manday.coreui.R
+
+data class TransitionAttributes(
+    var mode: Int = 0,
+    var colorScrim: Int = R.color.colorTransparent
+)
