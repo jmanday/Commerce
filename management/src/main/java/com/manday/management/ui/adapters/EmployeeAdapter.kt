064c6d63ae0c6f19c0b5ae86d136cc6459035a00
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-16 16:40:38 +0200

    [feature/COMMERCE-92] EmployeeAdapter updated with the new ViewHolder.

--- management/src/main/java/com/manday/management/ui/adapters/EmployeeAdapter.kt
+++ management/src/main/java/com/manday/management/ui/adapters/EmployeeAdapter.kt
@@ -3,15 +3,39 @@ package com.manday.employee.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.manday.coredata.utils.ordinal
 import com.manday.coreui.adapter.BaseAdapter
-import com.manday.employee.ui.viewholders.EmployeeViewHolder
 import com.manday.management.R
-import com.manday.management.domain.EmployeeModel
+import com.manday.management.ui.adapters.EmployeeItemAdapter
+import com.manday.management.ui.viewholders.EmployeeManagementViewHolder
 
-internal class EmployeeAdapter(listEmployee: List<EmployeeModel>, listener: (EmployeeModel, View) -> Unit): BaseAdapter<EmployeeModel, EmployeeViewHolder>(listEmployee, listener) {
+internal class EmployeeAdapter(
+    var listEmployee: List<EmployeeItemAdapter>,
+    listener: (EmployeeItemAdapter, View) -> Unit
+) : BaseAdapter<EmployeeItemAdapter, EmployeeManagementViewHolder>(listEmployee, listener) {
 
-    override fun generateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_item_employee, parent, false)
-        return EmployeeViewHolder(itemView)
+    override fun generateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): EmployeeManagementViewHolder {
+        return when (viewType) {
+            EmployeeItemAdapter.HeaderItemAdapter.ordinal() -> {
+                val itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.view_item_new_task, parent, false)
+                EmployeeManagementViewHolder.NewTaskViewHolder(itemView)
+            }
+            EmployeeItemAdapter.EmployeeModelItemAdapter.ordinal() -> {
+                val itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.view_item_employee, parent, false)
+                EmployeeManagementViewHolder.EmployeeViewHolder(itemView)
+            }
+            else -> {
+                throw IllegalStateException()
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return listEmployee.get(position).ordinal()
     }
 }
