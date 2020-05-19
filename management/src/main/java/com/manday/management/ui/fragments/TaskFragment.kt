71ee2b616a9e90dc3886471f7ea95fc5cd7009c5
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-19 14:29:46 +0200

    [feature/COMMERCE-80] Property NavigateFromTaskToDetailFragment added.

--- management/src/main/java/com/manday/management/ui/fragments/TaskFragment.kt
+++ management/src/main/java/com/manday/management/ui/fragments/TaskFragment.kt
@@ -10,15 +10,20 @@ import androidx.lifecycle.ViewModelProvider
 import com.manday.coreui.fragment.BaseFragment
 import com.manday.management.R
 import com.manday.management.databinding.FragmentTaskBinding
+import com.manday.management.navigation.NavigateFromTaskToDetailFragment
 import com.manday.management.ui.adapters.TaskAdapter
 import com.manday.management.ui.viewmodels.TaskViewModel
 import kotlinx.android.synthetic.main.fragment_task.*
+import org.koin.java.KoinJavaComponent.inject
 
 class TaskFragment : BaseFragment() {
 
     private val viewModel: TaskViewModel by lazy {
         ViewModelProvider(this).get(TaskViewModel::class.java)
     }
+    private val navigateToDetailFragment: NavigateFromTaskToDetailFragment by inject(
+        NavigateFromTaskToDetailFragment::class.java
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,12 +43,8 @@ class TaskFragment : BaseFragment() {
         viewModel.tasks?.observe(this, Observer { tasks ->
             taskRecyclerView.hideShimmer()
             if (tasks != null) {
-                taskRecyclerView.adapter = TaskAdapter(tasks) { taskEntity, view ->
-                    /*
-                    onItemClicked(R.id.bottom_to_top, Bundle().apply {
-                        putSerializable(ARGUMENT_EXTRA_TASK, it)
-
-                     */
+                taskRecyclerView.adapter = TaskAdapter(tasks) { taskModel, view ->
+                    navigateToDetailFragment.navigate(view, taskModel)
                 }
             } else {
                 showMessage(getString(R.string.error_get_datas), false)
