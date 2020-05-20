80ed7b4c913be5c7d1105acf7611919422a4942d
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-20 16:43:02 +0200

    [feature/COMMERCE-80] TaskDetailViewModel updated.

--- management/src/main/java/com/manday/management/ui/viewmodels/TaskDetailViewModel.kt
+++ management/src/main/java/com/manday/management/ui/viewmodels/TaskDetailViewModel.kt
@@ -1,26 +1,41 @@
 package com.manday.management.ui.viewmodels
 
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
 import com.manday.coredata.ExecutorViewModel
+import com.manday.coredata.utils.addSourceNotNull
+import com.manday.coredata.utils.removeSourceNotNull
 import com.manday.management.data.entities.TypeTaskEntity
 import com.manday.management.domain.EmployeeModel
+import com.manday.management.repository.EmployeeRepository
 import com.manday.management.repository.TaskRepository
 import org.koin.java.KoinJavaComponent.inject
 
 internal class TaskDetailViewModel : ExecutorViewModel() {
 
+    private val employeeRepository: EmployeeRepository by inject(EmployeeRepository::class.java)
     private val taskRepository: TaskRepository by inject((TaskRepository::class.java))
-    var listEmployees = MediatorLiveData<EmployeeModel>()
-    var typeTasks = MediatorLiveData<TypeTaskEntity>()
-
+    val listEmployees = MediatorLiveData<List<EmployeeModel>?>()
+    val typeTasks = MediatorLiveData<List<TypeTaskEntity>?>()
 
     init {
+        val source = doInBackground {
+            taskRepository.getAllTypeTasks()
+        }
+        typeTasks.addSourceNotNull(source, Observer {
+            typeTasks.removeSourceNotNull(source)
+            typeTasks.postValue(it)
+        })
 
     }
 
-
-    //fun getListEmployees() = employeeRepository.getEmployees()
-
-    fun typeTasks() = taskRepository.getAllTypeTasks()
-
+    fun setTypeTask(taskType: Int) {
+        val source = doInBackground {
+            employeeRepository.getEmployeeBySkill(taskType)
+        }
+        listEmployees.addSourceNotNull(source, Observer {
+            listEmployees.removeSourceNotNull(source)
+            listEmployees.postValue(it)
+        })
+    }
 }
