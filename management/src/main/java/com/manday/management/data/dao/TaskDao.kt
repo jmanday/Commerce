55460ef5c9881dd8ca40688973567f15e036b234
Jesús García Manday <manuel.garcia@edreamsodigeo.com>
2020-05-23 11:22:39 +0200

    [feature/COMMERCE-98] Method addTask in TaskDao update with type of return.

--- management/src/main/java/com/manday/management/data/dao/TaskDao.kt
+++ management/src/main/java/com/manday/management/data/dao/TaskDao.kt
@@ -1,7 +1,10 @@
 package com.manday.management.data.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.manday.management.data.entities.TaskEntity
 
 @Dao
@@ -14,7 +17,7 @@ interface TaskDao {
     fun getAllTasks(): LiveData<List<TaskEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun addTask(task: TaskEntity)
+    fun addTask(task: TaskEntity): Long?
 /*
     @Query("SELECT * FROM employees WHERE name = :mName AND pass = :mPass")
     fun getEmployee(mName: String, mPass: String): LiveData<Employee>
